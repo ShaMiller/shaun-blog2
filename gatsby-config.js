d59b6d385f64113f0a925fafa1f803af3c01890d
Pete Taylour <pete.taylour@gmail.com>
2020-08-01 12:52:14 +0100

    add rest of files

---
+++ gatsby-config.js
@@ -0,0 +1,87 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+}
+
+// if you want to use the preview API please define
+// CONTENTFUL_HOST in your environment config
+// the `host` property should map to `preview.contentful.com`
+// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST
+}
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the access token need to be provided."
+  )
+}
+
+module.exports = {
+  siteMetadata: {
+    title: `Shaun E. Miller`,
+    description: `Shaun E. Miller's personal site`,
+    author: `Shaun E. Miller`,
+  },
+  plugins: [
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-image`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-resolve-src`,
+    `gatsby-plugin-remove-trailing-slashes`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: "gatsby-source-prismic-graphql",
+      options: {
+        repositoryName: "prist", // (REQUIRED, replace with your own)
+        linkResolver: () => post => `/${post.uid}`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-prismic-starter-prist`,
+        short_name: `prist`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/oscar-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig,
+    },
+    // https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        head: true,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/templates/post.jsx
@@ -0,0 +1,193 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { graphql } from "gatsby"
+import styled from "@emotion/styled"
+import colors from "styles/colors"
+import Layout from "components/Layout"
+
+const PostHeroContainer = styled("div")`
+  max-height: 500px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  margin-bottom: 3em;
+
+  img {
+    width: 100%;
+  }
+`
+
+const PostHeroAnnotation = styled("div")`
+  padding-top: 0.25em;
+
+  h6 {
+    text-align: right;
+    color: ${colors.grey600};
+    font-weight: 400;
+    font-size: 0.85rem;
+  }
+
+  a {
+    color: currentColor;
+  }
+`
+
+const PostCategory = styled("div")`
+  max-width: 550px;
+  margin: 0 auto;
+  text-align: center;
+  font-weight: 600;
+  color: ${colors.grey600};
+
+  h5 {
+    margin-top: 0;
+    margin-bottom: 1em;
+  }
+`
+
+const PostTitle = styled("div")`
+  max-width: 550px;
+  margin: 0 auto;
+  text-align: center;
+
+  h1 {
+    margin-top: 0;
+  }
+`
+
+const PostBody = styled("div")`
+  max-width: 550px;
+  margin: 0 auto;
+
+  .block-img {
+    margin-top: 3.5em;
+    margin-bottom: 0.5em;
+
+    img {
+      width: 100%;
+    }
+  }
+`
+
+const PostMetas = styled("div")`
+  max-width: 550px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  margin-bottom: 2em;
+  justify-content: space-between;
+  font-size: 0.85em;
+  color: ${colors.grey600};
+`
+
+const PostAuthor = styled("div")``
+
+const PostDate = styled("div")``
+
+const Post = ({ post, meta }) => {
+  console.log(post)
+  return (
+    <>
+      <Helmet
+        title={`${post.title}`}
+        titleTemplate={`%s | ${meta.title}`}
+        meta={[
+          {
+            name: `description`,
+            content: meta.description,
+          },
+          {
+            property: `og:title`,
+            content: `${post.title}`,
+          },
+          {
+            property: `og:description`,
+            content: meta.description,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
+          },
+          {
+            name: `twitter:card`,
+            content: `summary`,
+          },
+          {
+            name: `twitter:creator`,
+            content: meta.author,
+          },
+          {
+            name: `twitter:title`,
+            content: meta.title,
+          },
+          {
+            name: `twitter:description`,
+            content: meta.description,
+          },
+        ].concat(meta)}
+      />
+      <Layout>
+        <PostCategory>{post.tags[0]}</PostCategory>
+        <PostTitle>
+          <h1>{post.title}</h1>
+        </PostTitle>
+        <PostMetas>
+          <PostAuthor>{meta.author}</PostAuthor>
+          <PostDate>{post.publishDate}</PostDate>
+        </PostMetas>
+        {post.heroImage && (
+          <PostHeroContainer>
+            <img {...post.heroImage.fluid} alt="bees" />
+            <PostHeroAnnotation>{post.imageCaption}</PostHeroAnnotation>
+          </PostHeroContainer>
+        )}
+        <PostBody
+          dangerouslySetInnerHTML={{
+            __html: post.body.childMarkdownRemark.html,
+          }}
+        ></PostBody>
+      </Layout>
+    </>
+  )
+}
+
+export default ({ data }) => {
+  const postContent = data.contentfulBlogPost
+  const meta = data.site.siteMetadata
+  return <Post post={postContent} meta={meta} />
+}
+
+Post.propTypes = {
+  post: PropTypes.object.isRequired,
+  meta: PropTypes.object.isRequired,
+}
+
+export const query = graphql`
+  query PostQuery($slug: String) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      tags
+      publishDate(formatString: "MMMM Do, YYYY")
+      #   imageCaption
+      heroImage {
+        fluid(maxWidth: 1180, background: "rgb:000000") {
+          ...GatsbyContentfulFluid_tracedSVG
+        }
+      }
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`
